d30e6fb873958c063c5e9031a0dde38ac4c1ff93
nisrinanm4 <ririnbantul@gmail.com>
2025-05-13 09:04:09 +0700

    Update register.php

--- register.php
+++ register.php
@@ -4,9 +4,9 @@ include 'koneksi.php';
 $message = [];
 
 if(isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $gender = $_POST['gender'];
-    $email = $_POST['email'];
+    $name = mysqli_real_escape_string($conn,$_POST['name']);
+    $gender = mysqli_real_escape_string($conn,$_POST['gender']);
+    $email = mysqli_real_escape_string($conn,$_POST['email']);
     $pass =  $_POST["password"];
     $cpass = $_POST['cpassword'];
     $image = $_FILES['image']['name'];
@@ -15,13 +15,14 @@ if(isset($_POST['submit'])){
     $image_folder = 'uploaded_img/'.$image;
 
     $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email'") or die('query failed');
+
     if (mysqli_num_rows($select) > 0) {
-        $message[] = 'Email sudah terdaftar!';
+        $message[] = 'user already exist!';
     } else {
         if ($pass != $cpass) {
-            $message[] = 'Konfirmasi password tidak cocok!';
+            $message[] = 'confirm password not matched!';
         } elseif ($image_size > 2000000) { 
-            $message[] = 'Ukuran gambar terlalu besar!';
+            $message[] = 'image size is too large!';
         } else {
             $hashed_pass = password_hash($pass, PASSWORD_DEFAULT);
 
@@ -29,9 +30,9 @@ if(isset($_POST['submit'])){
 
             if ($insert) {
                 move_uploaded_file($image_tmp_name, $image_folder);
-                $message[] = 'Registrasi berhasil!';
+                $message[] = 'registered succesfully!';
             } else {
-                $message[] = 'Registrasi gagal!';
+                $message[] = 'registeration failed!';
             }
         }
     }
@@ -50,34 +51,46 @@ if(isset($_POST['submit'])){
 <div class="container">
   <form action="" method="post" enctype="multipart/form-data" class="login-form">
   <h2>Register</h2>
-
+  <?php
+  if(isset($message)){
+    foreach($message as $msg){
+      echo '<div class = "message">'.$msg.'</div>';
+    }
+  }
+  ?>
+  <div class="flex">
   <div class="input-group">
-    <input type="text" name="name" required placeholder=" ">
+    <input type="text" name="name" required placeholder=" " />
     <label>Username</label>
-    <span class="icon">&#128100;</span>
   </div>
+</div>
+<div class="flex">
+  <div class="vertical-group">
   <div class="input-group">
-    <input type="text" name="gender" required placeholder=" ">
-    <label>Gender (male/female)</label><br>
-    </div>
+    <input type="text" name="gender" required placeholder=" " />
+    <label>Gender(male/female)</label>
+  </div>
   <div class="input-group">
     <input type="email" name="email" required placeholder=" ">
     <label>Email</label>
   </div>
+</div>
+</div>
+  <div class="flex">
+    <div class="vertical-group">
   <div class="input-group">
     <input type="password" name="password" required placeholder=" ">
     <label>Password</label>
   </div>
-
   <div class="input-group">
     <input type="password" name="cpassword" required placeholder=" ">
     <label>Confirm Password</label>
   </div>
-
   <div class="input-group">
-    <input type="file" name="image" id="image" accept="images/jpg" >
+    <input type="file" name="image" id="image" accept="image/jpeg, image/png, image/jpg" >
   </div>
-
+</div>
+</div>
   <button type="submit" name="submit">Register</button>
 
   <p>Already have an account? <a href="login.php">Login</a></p>
